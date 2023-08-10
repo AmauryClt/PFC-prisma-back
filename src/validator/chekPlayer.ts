d18db1ef755950551ee25b38b0e1f69c8d304fb6
CLOT Amaury <amauryclt@gmail.com>
2023-08-10 16:51:22 +0200

    backend V1 fini

--- src/validator/chekPlayer.ts
+++ src/validator/chekPlayer.ts
@@ -15,7 +15,7 @@ const checkNameMiddleware: RequestHandler = async (req, res, next) => {
 
     if (player) {
       //res.status(403).json({ error: "Le nom d'utilisateur existe déjà" });
-      res.send(player);
+      res.status(200).json({ existingUser: true, name: player.name });
     } else {
       next();
     }
