f10f70fdae3af886d0c7ea3cc232103a9a1fdcef
CLOT Amaury <amauryclt@gmail.com>
2023-08-10 16:04:39 +0200

    backend V1 fini

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import router from "./router";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors(
+  {origin: "http://localhost:5173"}
+));
 app.use(router);
 
 const server = app.listen(5000, () =>
