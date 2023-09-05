9a87a4049ba35e0894b7ec8f87396ad437e9ce45
CLOT Amaury <amauryclt@gmail.com>
2023-09-05 16:39:17 +0200

    commit pour deploy

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,14 @@ import cors from "cors";
 
 const app = express();
 
+
 app.use(express.json());
 app.use(cors(
   {origin: process.env.FRONTEND_URL}
 ));
 app.use(router);
 
-const server = app.listen(5000, () =>
+const server = app.listen(process.env.APP_PORT, () =>
   console.log(`
 🚀 Server ready at: process.env.APP_PORT
 ⭐️ See sample requests: http://pris.ly/e/ts/rest-express#3-using-the-rest-api`)
